5692a9c66e903a5007136eae11282815c8345134
snototter <h.possegger+dev@gmail.com>
2019-12-21 14:45:31 +0100

    documentation

--- libraries/BasicTinkering/Button.h
+++ libraries/BasicTinkering/Button.h
@@ -7,9 +7,17 @@
 class Button
 {
 public:
-  //TODO comment
-  // notify_hold_once: isHeld() will return true only once (unless button is released and pressed again)
-  Button(uint8_t pin, unsigned int debounce_delay = 50, unsigned int hold_delay = 500, bool notify_hold_once = true);
+  // pin:              Pin number
+  // debounce_delay:   How many milliseconds to wait for stabilization of
+  //                   button signal. Set to 0 to disable/use a hardware-
+  //                   debounce (e.g. Schmitt trigger).
+  // hold_delay:       The button must be pressed for X milliseconds before
+  //                   isHeld() will return true.
+  // notify_hold_once: If true, isHeld() returns true only once (unless button
+  //                   is released and pressed again). Otherwise, isHeld()
+  //                   returns true as long as the button is pressed.
+  Button(uint8_t pin, unsigned int debounce_delay = 50, 
+    unsigned int hold_delay = 500, bool notify_hold_once = true);
 
   // Must be invoked in every loop iteration to scan the button's pin.
   // Returns true if the button is currently pressed.
@@ -34,10 +42,16 @@ private:
   // Internal state variable
   uint8_t state_;
 
+  // How long to wait for signal stabilization
   unsigned int debounce_delay_;
+
+  // Time stamp of last signal change
   unsigned long debounce_start_;
 
+  // How long to wait before considering isHeld() to be true
   unsigned int hold_delay_;
+
+  // Time stamp of last button down
   unsigned long hold_start_;
 };
 #endif
